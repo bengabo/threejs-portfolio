94d7a9db4c00e86ccc599ee263269832311e62c7
bengabo <ben@bengabo.com>
2023-09-19 08:41:26 +0200

    motion with mouse scroll

--- src/script.js
+++ src/script.js
@@ -1,5 +1,9 @@
+import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass.js";
+import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer.js";
+import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js";
 import * as dat from "lil-gui";
 
 /**
@@ -9,10 +13,72 @@ import * as dat from "lil-gui";
 const gui = new dat.GUI();
 
 // Canvas
-const canvas = document.querySelector("canvas.webgl");
+const canvas = document.querySelector("#bg");
 
 // Scene
 const scene = new THREE.Scene();
+
+//Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  1000
+);
+camera.position.x = -3;
+camera.position.z = 30;
+scene.add(camera);
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+const bloomPass = new UnrealBloomPass(
+  new THREE.Vector2(sizes.width, sizes.height),
+  1.5,
+  0.4,
+  0.85
+);
+bloomPass.threshold = 0;
+bloomPass.strength = 0.6;
+const renderScene = new RenderPass(scene, camera);
+const composer = new EffectComposer(renderer);
+composer.setPixelRatio(2);
+composer.addPass(renderScene);
+composer.addPass(bloomPass);
+
+const group = new THREE.Group();
+scene.add(group);
+
+composer.setSize(sizes.width, sizes.height);
+bloomPass.setSize(sizes.width, sizes.height);
 
 /**
  * Textures
@@ -28,15 +94,26 @@ scene.background = spaceTexture;
  * Geometries
  */
 // Torus
-const torus = new THREE.Mesh(
-  new THREE.TorusGeometry(10, 3, 16, 100),
-  new THREE.MeshStandardMaterial({
-    color: 0xff6347,
-  })
-);
-// scene.add(torus);
+const torus = (radius, tube, radialSegments, tubularSegments, hexaColor) => {
+  const torusGeometry = new THREE.TorusGeometry(
+    radius,
+    tube,
+    radialSegments,
+    tubularSegments
+  );
+  const torusMaterial = new THREE.MeshBasicMaterial({ color: hexaColor });
+  const torusMesh = new THREE.Mesh(torusGeometry, torusMaterial);
+  return torusMesh;
+};
 
-// Star
+const torus1 = torus(6, 0.05, 8, 8, 0xffffff);
+const torus2 = torus(6, 0.05, 8, 8, 0xffffff);
+torus2.rotation.set(1, 1, 1);
+const torus3 = torus(6, 0.05, 8, 8, 0xffffff);
+torus3.rotation.set(2.5, 2.5, 2.5);
+scene.add(torus1, torus2, torus3);
+
+// Stars field
 const addStar = () => {
   const star = new THREE.Mesh(
     new THREE.SphereGeometry(0.25),
@@ -58,7 +135,9 @@ const cube = new THREE.Mesh(
     map: cubeTexture,
   })
 );
-// scene.add(cube);
+cube.rotation.y = -15;
+cube.rotation.x = 0.5;
+scene.add(cube);
 
 const helper = new THREE.BoxHelper(cube);
 helper.material.color.setHex(0x474747);
@@ -66,20 +145,24 @@ helper.material.blending = THREE.AdditiveBlending;
 helper.material.transparent = true;
 // scene.add(helper);
 
-// Planet
+// Moon
 const moonTexture = textureLoader.load("./img/Moon_Diffuse_4000x2000px.jpg");
-const moonDispTexture = textureLoader.load("./img/MOON_ldem_3_8bit.jpg");
-const moonNormTexture = textureLoader.load("./img/Moon_Normal_4000x2000px.jpg");
+const moonNormTexture1 = textureLoader.load(
+  "./img/Moon_Normal_4000x2000px.jpg"
+);
+const moonNormTexture2 = textureLoader.load("./img/moon_normal.jpg");
 const moon = new THREE.Mesh(
   new THREE.SphereGeometry(10, 32, 32),
   new THREE.MeshStandardMaterial({
-    map: moonNormTexture,
-    normalMap: moonNormTexture,
+    map: moonNormTexture1,
+    normalMap: moonNormTexture1,
     color: "#CE65ED",
     // displacementMap: moonDispTexture,
   })
 );
 scene.add(moon);
+moon.position.z = 30;
+moon.position.x = -10;
 
 /**
  * Lights
@@ -88,59 +171,36 @@ const pointLight = new THREE.PointLight(0xffffff, 1.5);
 pointLight.position.set(13, 5, 15);
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.15);
 scene.add(pointLight, ambientLight);
+
+/**
+ * Helpers
+ */
 const lightHelper = new THREE.PointLightHelper(pointLight);
 const gridHelper = new THREE.GridHelper(200, 50);
-scene.add(lightHelper);
+scene.add(lightHelper, gridHelper);
 
 /**
- * Sizes
+ * Scroll function
  */
-const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
-};
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+  moon.rotation.x += 0.05;
+  moon.rotation.y += 0.075;
+  moon.rotation.z += 0.05;
 
-window.addEventListener("resize", () => {
-  // Update sizes
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.rotation.y = t * -0.0002;
+}
 
-  // Update camera
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
-
-  // Update renderer
-  renderer.setSize(sizes.width, sizes.height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-});
-
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(
-  75,
-  sizes.width / sizes.height,
-  0.1,
-  1000
-);
-camera.position.z = 30;
-scene.add(camera);
+document.body.onscroll = moveCamera;
+moveCamera();
 
 // Controls
-const controls = new OrbitControls(camera, canvas);
-controls.enableDamping = true;
-controls.minDistance = 25;
-controls.maxDistance = 25;
-
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
-});
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+// const controls = new OrbitControls(camera, canvas);
+// controls.enableDamping = true;
+// controls.minDistance = 25;
+// controls.maxDistance = 25;
 
 /**
  * Animate
@@ -150,12 +210,13 @@ const clock = new THREE.Clock();
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
 
-  torus.rotation.x += 0.001;
-  torus.rotation.y += 0.001;
-  torus.rotation.z += 0.001;
+  // torus1.rotation.x += 0.01;
+  // torus1.rotation.y += 0.005;
+  // torus1.rotation.z += 0.01;
+  moon.rotation.y -= 0.0005;
 
   // Update controls
-  controls.update();
+  // controls.update();
 
   // Render
   renderer.render(scene, camera);
